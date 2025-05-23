b30b1dc31426f715b61001734431ee7617a53bc1
MdMehedi-Hasan <mh19115@gmail.com>
2025-05-24 00:46:04 +0600

    table design fix

--- components/admin/posts/table.jsx
+++ components/admin/posts/table.jsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import {
   Table,
   TableBody,
@@ -6,30 +7,43 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { AiOutlineDelete } from "react-icons/ai";
+import { LuPencilLine } from "react-icons/lu";
 
-const PostsTable = ({posts}) => {
+const PostsTable = ({ posts }) => {
   return (
-    <div className="w-full overflow-hidden">
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead className="text-center">ID</TableHead>
-            <TableHead className="text-center">User ID</TableHead>
-            <TableHead className="text-center">Title</TableHead>
-            <TableHead className="text-center">Body</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
+    <div className="w-full overflow-x-auto">
+      <table class="w-full table-auto border border-gray-300">
+        <thead class="bg-gray-100">
+          <tr>
+            <th class="px-4 py-2 text-center border">ID</th>
+            <th class="px-4 py-2 text-center border">User&nbsp;ID</th>
+            <th class="px-4 py-2 text-center border">Title</th>
+            <th class="px-4 py-2 text-center border">Body</th>
+            <th class="px-4 py-2 text-center border">Action</th>
+          </tr>
+        </thead>
+        <tbody>
           {posts?.map((post) => (
-            <TableRow key={post.id}>
-              <TableCell className="font-medium text-center">{post.id}</TableCell>
-              <TableCell className="text-center">{post.userId}</TableCell>
-              <TableCell>{post.title}</TableCell>
-              <TableCell>{post.body}</TableCell>
-            </TableRow>
+            <tr>
+              <td class="px-4 py-2 text-center border">{post?.id}</td>
+              <td class="px-4 py-2 text-center border">{post?.userId}</td>
+              <td class="px-4 py-2 border break-words">{post?.title}</td>
+              <td class="px-4 py-2 border break-words">{post?.body}</td>
+              <td class="px-4 py-2 border break-words">
+                <div className="flex items-center gap-2">
+                  <Button variant="outline" className="text-blue-500 border border-blue-500 hover:bg-blue-500 hover:text-white">
+                    <LuPencilLine/>
+                  </Button>
+                  <Button variant="outline" className="text-red-500 border border-red-500 hover:bg-red-500 hover:text-white">
+                    <AiOutlineDelete />
+                  </Button>
+                </div>
+              </td>
+            </tr>
           ))}
-        </TableBody>
-      </Table>
+        </tbody>
+      </table>
     </div>
   );
 };
